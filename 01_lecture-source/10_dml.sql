1e3f9a8425e9e966fd73f08d26460a886c3f38b4
playdrew <playdrew776p@gmail.com>
2024-09-25 15:57:58 +0900

    [RE]

--- 01_lecture-source/10_dml.sql
+++ 01_lecture-source/10_dml.sql
@@ -72,5 +72,83 @@ set
 where
 	menu_code = 23;
     
--- delete나 update는 조건을 설정하는 것이에요 조건안정하면 다 바꿔버릴지도
+-- delete나 update는 조건을 설정하는 것이에요 조건안정하면 다 바꿔버림
+
+-- subquery를 사용해서 update
+-- 주의점. update 나 delete 시에 자기 자신의 테이블의 데이터를 
+-- 사용하게 되면 1093 error 발생
+
+-- 문제.
+-- 메뉴의 이름이 파인애플탕인 메뉴의 카테고리코드를 6으로 수정
+-- where menu_name = '파인애플탕' x
+-- where menu_code 를 통해 파인애플탕 추론하기
+-- 확인없이 파인애플탕의 menu_code를 알수가 없어요
+update tbl_menu
+set
+	category_code = 6
+where 
+	menu_code = (
+				 select
+					menu_code
+				 from
+					tbl_menu
+				 where
+					menu_name = '파인애플탕'
+				);
+                
+-- 1093 error 문제해결 : subquery의 from 절에서 tbl_menu 를 사용한게 문제 -> cte 를 사용해 해결
+
+update tbl_menu
+set
+	category_code = 6
+where 
+	menu_code = (
+				 select
+					cte.menu_code
+				 from
+					(
+					 select
+						menu_code
+					 from
+						tbl_menu
+					 where
+						menu_name = '파인애플탕'
+                    ) cte
+				);
+
+-- delete
+-- 테이블의 행을 삭제하는 구문이다.
+-- 테이블의 행의 갯수가 줄어든다.
+
+-- limit
+delete from tbl_menu
+order by menu_price -- 아무것도 안적혀있으면 asc 가 디폴트값
+limit 2;
+
+select * from tbl_menu;
+
+-- where 사용으로 단일 행 삭제
+delete 
+from
+	tbl_menu
+where
+	menu_code = 26;
     
+delete from tbl_menu; -- 해당하는 테이블 자체가 날라간게 아닌 테이블안의 데이터가 날라간 것 입니다. 복구못합니다. 
+
+-- 슬레이브db 와 마스터db가 있는데 슬레이브 복제본을 떠서 유지하는 경우가 많다고 합니다.
+-- 조회라는 것 join 이 조금 위험해요 테이블을 얽고 얽는데 시간이 오래 걸려요 나중에는 테이블이 얽히고 섫혀있어서 시간이 오래걸려요 테이블에 대한 정보를 읽기때문입니다.
+-- 나중엔 조회용 db 하나와 dml 수정/삭제/삽입 을 위한 db 를 하는 경우도 있습니다.
+-- 쿼리문을 마치 객체처럼 호출해요. 메뉴코드만 전달인자로 쿼리객체를 변수로 전달을 하면은 삭제하는 식으로 동작합니다?
+-- 단일 행을 삭제할때 반드시 한 행을 삭제할 수 있는 유일한 값으로 삭제를 해야합니다.그것을 pk라고 합니다.
+-- 게시글 삭제했는데 게시글 5개가 삭제되는 경우 삭제를 할때 내가 가진 이름으로 삭제해야한다라고 할 경우 문제생김?
+
+-- replace
+-- insert 시 primary key 또는 unique key 가 충돌이 발생핝다면
+-- replace 를 통해 중복 된 데이터를 덮어 쓸 수 있다.
+
+insert into tbl_menu values (15,'소주',6000,10,'Y');
+-- insert into tbl_menu values (15,'소주',6000,10,'Y'); pk 값이 중복되면서 오류가 발생하는데요
+replace into tbl_menu values (15,'소주',8000,10,'Y'); -- replace는 중복되면 덮어씁니다 pk의 하위 unique key 제약조건을 id같은 것에 많이 걸어둡니다 replace 를 하면 조금 더 안전하게 실행을 시킬 수 있다. 삭제라기보단 업데이트 2rows affected 기존의 것 새로운 것2번
+select * from tbl_menu;
+
